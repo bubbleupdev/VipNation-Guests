059f88ad27f072b0d24bd74c10eb083720307b35
Brian Clark <brianc@fastmail.com>
2023-06-21 10:51:07 -0500

    app icons favicons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,24 @@ const config: CapacitorConfig = {
   appId: 'com.VIPNation.GuestLists',
   appName: 'VIPNation Guests List',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#000000ff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#E21836",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
