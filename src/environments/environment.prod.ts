f55352228a4f0d064bd5e3430adfbaad7185b6ad
rustem <rustem@icoosoft.com>
2023-10-09 19:41:06 +0500

    enable prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  graphQlEndpointUrl: 'https://vipnation.7.dev.bubbleup.com/api/guests/graph',
+  // graphQlEndpointUrl: 'https://vipnation.7.dev.bubbleup.com/api/guests/graph',
+  graphQlEndpointUrl: 'https://vipnation.7.prod.bubbleup.com/api/v1/graph',
   siteName: "VIP Nation Guests",
   storageName: 'vipnDataStore',
   updatePeriod: 120
